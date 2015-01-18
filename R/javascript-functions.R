301355413ce71811479a27c2e18d78e422671883
yutannihilation <yutani.ini@gmail.com>
2015-01-18 02:47:07 +0000

    add a helper function to generate Javascript functions for labelInterporateFnc option

---
+++ R/javascript-functions.R
@@ -0,0 +1,40 @@
+#' Helper Functions to Generate Javascript Functions
+#' 
+#' @name javascript
+NULL
+
+#' A Helper Function For labelInterporationFnc
+#' 
+#' @rdname javascript
+#' 
+#' @param interval interval between labels
+#' @param offset   offset to the first label
+#' @param prefix   prefix of labels (e.g. $)
+#' @param suffix   suffix of labels (e.g. kB)
+#' 
+#' @examples
+#' \dontrun{
+#' #' set.seed(324)
+#' data <- data.frame(
+#'   day = 1:20,
+#'   A   = runif(20, 0, 10),
+#'   B   = runif(20, 0, 10),
+#'   C   = runif(20, 0, 10),
+#'   D   = runif(20, 0, 10)
+#'   )
+#' 
+#' chartist(data, day) + Line(x_labelInterpolationFnc = create_labelInterporationFnc(4, offset = 2))
+#' 
+#' chartist(data, day) + Line(x_labelInterpolationFnc = create_labelInterporationFnc(4, prefix = "day "))
+#' }
+#' 
+#' @export
+create_labelInterporationFnc <- function(interval = 1, offset = 0, prefix = NULL, suffix = NULL) {
+  if(is.numeric(interval) && length(interval) != 1) stop("interval must be single numeric value")
+  if(is.numeric(offset) && length(offset) != 1)     stop("offset must be single numeric value")
+  
+  # NULL is automatically removed by c()
+  values <- c(sprintf('"%s"', prefix), "value", sprintf('"%s"', suffix))
+  htmlwidgets::JS(sprintf("function(value, index) {return (index - %d) %% %d === 0 ? %s : null;}",
+                          offset, interval, paste(values, collapse = " + ")))
+}
